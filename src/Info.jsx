f00e25b4e4c485e8ac7301a98d0a1ccea74caafb
Vince Williams <williamsvm20@gmail.com>
2020-06-11 22:21:24 -0400

    fixed the document name in index.html and added the date to the info component.

--- src/Info.jsx
+++ src/Info.jsx
@@ -2,10 +2,11 @@ import React from "react"
 
 const Info = (props) => {
     const {nasaData} = props
-    const {explanation} = nasaData
+    const {date, explanation} = nasaData
 
     return (
         <div>
+            <date>{date}</date>
             <p>{explanation}</p>
         </div>
     )
