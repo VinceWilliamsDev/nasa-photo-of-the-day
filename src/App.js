f90ac69de9584886c882ba645c8e035f39b1bfc6
Vince Williams <williamsvm20@gmail.com>
2020-06-11 22:23:10 -0400

    turned off console logs in App.js.

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ import Info from "./Info"
 function App() {
 
   const [nasaData, setNasaData] = useState([])
-  console.log(nasaData)
+  // console.log(nasaData)
   
   useEffect(() => {
     axios.get("https://api.nasa.gov/planetary/apod?api_key=rEPFFdc3h8b8XKRPqwkxwm1h2AiHZGtUFi2cTl1q")
       .then(res => {
-        console.log(res.data)
+        // console.log(res.data)
         setNasaData(res.data)
       })
       .catch(err => {
